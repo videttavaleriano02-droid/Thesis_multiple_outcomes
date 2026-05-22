312c903be288d12bfa326957c7a2d06e70877044
Valeriano <videttavaleriano02@gmail.com>
2026-05-22 16:10:29 +0200

    added R file split and impute

---
+++ split_impute.R
@@ -0,0 +1,85 @@
+# ================================================================================================ #
+#                                         SPLIT & IMPUTE                                           #
+# ================================================================================================ #
+
+rm(list = ls())
+setwd('/Users/valerianovidetta/Desktop/Tesi/Dataset/') 
+
+# libraries:
+library(tidyverse)
+library(caret)
+
+# -------------
+
+# functions:
+
+split_random <- function(df, p = 0.70, seed = 1) {
+  set.seed(seed)
+  idx <- createDataPartition(as.factor(df$sesso_E1), p = p, list = FALSE)
+  list(train = df[idx, ], test = df[-idx, ])
+}
+
+
+# --------------------------------------------------------------------------------------------------
+
+# import dataset
+
+original <- readxl::read_xlsx('strategy_firenze.xlsx')
+
+                                    # ------------------ #
+                                    # HOLD-OUT SPLITTING #
+                                    # ------------------ #
+
+split <- split_random(original, p = .8, seed = 272727)
+
+develop <- split$train
+test <- split$test
+
+write.csv(develop, "development.csv")
+write.csv(test, "test.csv")
+
+
+                                    # ------------------ #
+                                    #  CROSS VALIDATION  #
+                                    # ------------------ #
+
+
+folds <- createFolds(rownames(develop), k = 5, list = T, returnTrain = F)
+
+dir.create('folds_csv', showWarnings = F)
+
+folds_spl <- lapply(folds,
+            function(x){
+              validation <- develop[x,]
+              train <- develop[-x,]
+              list(train = train, validation = validation) 
+            }
+)
+
+for (i in seq_along(folds_spl)) {
+  write.csv(folds_spl[[i]]$train,      paste0("folds_csv/train_fold", i, ".csv"), row.names = F)
+  write.csv(folds_spl[[i]]$validation, paste0("folds_csv/val_fold",   i, ".csv"), row.names = F)
+}
+
+
+                                    # ------------------ #
+                                    #     IMPUTATION     #
+                                    # ------------------ #
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
